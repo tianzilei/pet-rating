4ec55731aa039a4676b01501125b27f94a2f4d36
Ossi Laine <ossi.laine@utu.fi>
2019-03-13 13:59:18 +0200

    Breaked 'task/<int:page_num>/' GET and POST requests to own methods

--- app/task/views.py
+++ app/task/views.py
@@ -30,7 +30,77 @@ task_blueprint = Blueprint("task", __name__,
                 static_folder='static',
                 url_prefix='/task')
 
-@task_blueprint.route('/<int:page_num>', methods=['GET', 'POST'])
+
+def get_randomized_page(pages):
+
+    #this variable is feeded to the template as empty if trial randomization is set to "off"
+    randomized_stimulus = ""
+
+    #if trial randomization is on we will still use the same functionality that is used otherwise
+    #but we will pass the randomized pair of the page_id from trial randomization table to the task.html
+    randomized_page_id = trial_randomization.query.filter(and_(
+            trial_randomization.answer_set_idanswer_set==session['answer_set'], 
+            trial_randomization.page_idpage==pages.items[0].idpage
+        )).first()
+
+    return randomized_page_id
+
+
+def add_slider_answer(key, value, randomized_page_id):
+    '''Insert slider value to database. If trial randomization is set to 'Off' 
+    the values are inputted for session['current_idpage']. Otherwise the values 
+    are set for the corresponding id found in the trial randomization table'''
+
+    page_idpage = session['current_idpage'] if session['randomization'] == 'Off' else randomized_page_id.randomized_idpage 
+    participant_answer = answer(question_idquestion=key, answer_set_idanswer_set=session['answer_set'], answer=value, page_idpage=page_idpage)
+    db.session.add(participant_answer)
+    db.session.commit()
+
+
+@task_blueprint.route('/<int:page_num>', methods=['POST'])
+def task_answer(page_num):
+
+    form = TaskForm(request.form)
+    pages = page.query.filter_by(experiment_idexperiment=session['exp_id']).paginate(per_page=1, page=page_num, error_out=True)
+
+    # TODO: determine wheter handling POST data from slider or embody!!!
+
+    if form.validate():
+        #Lets check if there are answers in database already for this page_id (eg. if user returned to previous page and tried to answer again)
+        #If so flash ("Page has been answered already. Answers discarded"), else insert values in to db
+        #this has to be done separately for trial randomization "on" and "off" situations        
+
+        if session['randomization'] == 'On':
+            randomized_page_id = get_randomized_page(pages)
+            check_answer = answer.query.filter(and_(answer.answer_set_idanswer_set==session['answer_set'], answer.page_idpage==randomized_page_id.randomized_idpage)).first()
+        else:
+            check_answer = answer.query.filter(and_(answer.answer_set_idanswer_set==session['answer_set'], answer.page_idpage==session['current_idpage'])).first()
+
+        if check_answer is None:
+            the_time = datetime.now()
+            the_time = the_time.replace(microsecond=0)
+            
+            update_answer_counter = answer_set.query.filter_by(idanswer_set=session['answer_set']).first()
+            update_answer_counter.answer_counter = int(update_answer_counter.answer_counter) + 1 
+            update_answer_counter.last_answer_time = the_time
+            db.session.commit()
+        
+            data = request.form.to_dict()
+            for key, value in data.items():
+                add_slider_answer(key, value, randomized_page_id)
+
+        else:
+            flash("Page has been answered already. Answers discarded")
+        
+        page_num=pages.next_num
+        
+        if not pages.has_next:
+            return redirect ( url_for('task.completed'))
+
+    return redirect( url_for('task.task', page_num=pages.next_num))
+
+
+@task_blueprint.route('/<int:page_num>', methods=['GET'])
 def task(page_num):
 
     experiment_info = experiment.query.filter_by(idexperiment=session['exp_id']).first()
@@ -49,14 +119,12 @@ def task(page_num):
     #this variable is feeded to the template as empty if trial randomization is set to "off"
     randomized_stimulus = ""
 
-    #if trial randomization is on we will still use the same functionality that is used otherwise
-    #but we will pass the randomized pair of the page_id from trial randomization table to the task.html
+    # if trial randomization is on we will still use the same functionality that is used otherwise
+    # but we will pass the randomized pair of the page_id from trial randomization table to the task.html
     if session['randomization'] == 'On':
-        randomized_page_id = trial_randomization.query.filter(and_(trial_randomization.answer_set_idanswer_set==session['answer_set'], trial_randomization.page_idpage==pages.items[0].idpage)).first()
-        #answer.query.filter(and_(answer.answer_set_idanswer_set==session['answer_set'], answer.page_idpage==session['current_idpage'])).first()
-        #flash("randomized page:")
-        #flash(randomized_page_id.randomized_idpage)
+
         #set the stimulus to be shown if randomization is on
+        randomized_page_id = get_randomized_page(pages)
         randomized_stimulus = page.query.filter_by(idpage=randomized_page_id.randomized_idpage).first()
         
     for p in pages.items:
@@ -73,62 +141,16 @@ def task(page_num):
     
     form.categories1 = categories_and_scales
 
-    #slider set form handling
-    if request.method == 'POST'and form.validate():
-        #Lets check if there are answers in database already for this page_id (eg. if user returned to previous page and tried to answer again)
-        #If so flash ("Page has been answered already. Answers discarded"), else insert values in to db
-        #this has to be done separately for trial randomization "on" and "off" situations        
-        
-        if session['randomization'] == 'On':
-            check_answer = answer.query.filter(and_(answer.answer_set_idanswer_set==session['answer_set'], answer.page_idpage==randomized_page_id.randomized_idpage)).first()
-        
-        if session['randomization'] == 'Off':
-            check_answer = answer.query.filter(and_(answer.answer_set_idanswer_set==session['answer_set'], answer.page_idpage==session['current_idpage'])).first()
-
-        if check_answer is None:
-            
-            the_time = datetime.now()
-            the_time = the_time.replace(microsecond=0)
-            
-            update_answer_counter = answer_set.query.filter_by(idanswer_set=session['answer_set']).first()
-            update_answer_counter.answer_counter = int(update_answer_counter.answer_counter) + 1 
-            update_answer_counter.last_answer_time = the_time
-        
-            #flash("vastauksia:")
-            #flash(update_answer_counter.answer_counter)
-            db.session.commit()
-        
-            data = request.form.to_dict()
-            for key, value in data.items():
-                #flash(key)
-                #flash(value)
-                #flash(session['current_idpage'])
-            
-                #Insert slider values to database
-                
-                #If trial randomization is set to 'Off' the values are inputted for session['current_idpage']
-                #Otherwise the values are set for the corresponding id found in the trial randomization table
-                
-                if session['randomization'] == 'Off':
-                    participant_answer = answer(question_idquestion=key, answer_set_idanswer_set=session['answer_set'], answer=value, page_idpage=session['current_idpage'])
-                    db.session.add(participant_answer)
-                    db.session.commit()
-                else:
-                    participant_answer = answer(question_idquestion=key, answer_set_idanswer_set=session['answer_set'], answer=value, page_idpage=randomized_page_id.randomized_idpage)
-                    db.session.add(participant_answer)
-                    db.session.commit()
-
-        else:
-            flash("Page has been answered already. Answers discarded")
-        
-        page_num=pages.next_num
-        
-        if pages.has_next:
-            return redirect( url_for('task.task', page_num=pages.next_num))
-        
-        return redirect ( url_for('task.completed'))
-
-    return render_template('task.html', pages=pages, progress_bar_percentage=progress_bar_percentage, form=form, randomized_stimulus=randomized_stimulus, rating_instruction=rating_instruction, stimulus_size=stimulus_size, stimulus_size_text=stimulus_size_text)
+    return render_template(
+                        'task.html', 
+                        pages=pages, 
+                        progress_bar_percentage=progress_bar_percentage, 
+                        form=form, 
+                        randomized_stimulus=randomized_stimulus, 
+                        rating_instruction=rating_instruction, 
+                        stimulus_size=stimulus_size, 
+                        stimulus_size_text=stimulus_size_text
+                        )
 
 
 @task_blueprint.route('/completed')
@@ -156,7 +178,7 @@ def continue_task():
         participant = answer_set.query.filter(and_(answer_set.session==form.participant_id.data, answer_set.experiment_idexperiment==exp_id)).first()
         if participant is None:
             flash('Invalid ID')
-            return redirect(url_for('task.continue', exp_id=exp_id))        
+            return redirect(url_for('task.continue_task', exp_id=exp_id))        
         
         #flash('Login requested for participant {}'.format(form.participant_id.data))
         
